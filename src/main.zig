152b81714e73fc32e9294893ed6e8a2710dc8ca9
Sublime Tshimpangila <tshimangasublime@gmail.com>
2026-04-11 20:06:57 -0400

    remove dead comments

--- src/main.zig
+++ src/main.zig
@@ -17,7 +17,6 @@ pub fn main() !void {
     var lexer = Lexer.init(source_code, tokenStr);
 
     while (lexer.next()) {
-        // std.debug.print("Token: {}, value: {s}\n", .{ lexer.token, lexer.name.items });
         std.debug.print("Token: {t:<15} value: {s:<20}\n", .{ lexer.token, lexer.name.items });
         if (lexer.token == .TokenEnd) break;
     }
