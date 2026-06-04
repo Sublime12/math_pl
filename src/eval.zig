4bef062e8b67f906b80d7a430192e46b46a1775e
Sublime Tshimpangila <tshimangasublime@gmail.com>
2026-06-03 21:11:48 -0400

    implement eval for field_access

--- src/eval.zig
+++ src/eval.zig
@@ -219,7 +219,13 @@ pub fn eval(expr: Expr, ctx: Context, local_vars: Vars) Expr {
             }
             return .{ .struct_instance = new_expr };
         },
-        .field_access => panic("eval not yet implemented for field_access", .{}),
+        .field_access => |field_access| {
+            const elhs = eval(field_access.lhs.*, ctx, local_vars);
+            assert(elhs.tag() == .struct_instance);
+            return elhs.struct_instance.fields.get(field_access.field) orelse {
+                panic("tried accessing {s} in struct {s} but do not exist", .{field_access.field, elhs.struct_instance.name});
+            };
+        },
         .bind => |bind| return eval_bind(bind, ctx, local_vars),
         .void_ => return expr,
     }
